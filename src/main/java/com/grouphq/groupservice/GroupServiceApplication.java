bf850ba83e2618b222e8d099cee87dc49cc3dc86
Mohamed Mohamed <therealmohamedmohamed@gmail.com>
2023-08-28 15:43:13 -0400

    Added several checkers for code style, code quality, and dependency vulnerabilities

--- src/main/java/com/grouphq/groupservice/GroupServiceApplication.java
+++ src/main/java/com/grouphq/groupservice/GroupServiceApplication.java
@@ -2,12 +2,24 @@ package com.grouphq.groupservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
+/**
+ * Application Setup.
+ *
+ * @author makmn
+ *
+ */
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class GroupServiceApplication {
 
-    public static void main(String[] args) {
+    /**
+     * Entry point to application.
+     *
+     * @param args Command-line arguments
+     */
+    public static void main(final String[] args) {
         SpringApplication.run(GroupServiceApplication.class, args);
     }
-
 }
--- src/test/java/com/grouphq/groupservice/GroupServiceApplicationTests.java
+++ src/test/java/com/grouphq/groupservice/GroupServiceApplicationTests.java
@@ -1,13 +1,27 @@
 package com.grouphq.groupservice;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+/**
+ * Test setup.
+ *
+ * @author makmn
+ *
+ */
 @SpringBootTest
 class GroupServiceApplicationTests {
 
+    @Autowired
+    private ApplicationContext applicationContext;
+
     @Test
     void contextLoads() {
+        assertThat(applicationContext).isNotNull();
     }
 
 }
